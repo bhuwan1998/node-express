4e291eced668cd328b882aa846e79f13c4b97e79
bhuwan1998 <rfperfect785@gmail.com>
2020-12-28 12:07:31 +1100

    Express Server

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express'); // require express from node modules 
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000; 
 
 const app = express();
 
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname+'/public')); // in the root folder -> static html files 
+
+
 app.use((req, res, next) => {
     // next is used when you need to use additional middleware 
     // optional parameter 
-    console.log(req.headers);
+    // console.log(req.headers); -> remove the log because morgan will do it for us 
+
     res.statusCode = 200; 
     res.setHeader('Content-type', 'text/html');
     res.end('<html><body><h1> This is an express server </h1></body> </html>');
